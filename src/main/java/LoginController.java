3363ebdfbab85ad810cdc355d3fea080b021f62b
Gabriela Zarnescu <zarnescu_gabriela@yahoo.com>
2020-05-11 16:17:28 +0300

    subtask JOSMA-28 completed "Create the registration form"

--- src/main/java/LoginController.java
+++ src/main/java/LoginController.java
@@ -1,11 +1,40 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
-public class LoginController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @FXML
+    private AnchorPane rootPane ;
+
 
     @FXML
     void click(ActionEvent event) {
 
     }
+    @FXML
+    void register(ActionEvent event) throws IOException
+    {
+       AnchorPane pane = FXMLLoader.load(getClass().getResource("register.fxml"));
+       rootPane.getChildren().setAll(pane);
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    void method(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("login.fxml"));
+        mainPane.getChildren().setAll(pane);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("FIS - Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
+
         primaryStage.show();
 
     }
