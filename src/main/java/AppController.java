5d00ead738c278407fb7f9ca169d580c4a2eb5fe
Gabriela Zarnescu <zarnescu_gabriela@yahoo.com>
2020-06-08 17:32:15 +0300

    JOSMA-11 menu created

--- src/main/java/AppController.java
+++ src/main/java/AppController.java
@@ -1,2 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class AppController {
+    @FXML
+    private AnchorPane anchor;
+
+    public void products(javafx.event.ActionEvent actionEvent) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("cart.fxml"));
+        anchor.getChildren().setAll(pane);
+    }
+
+    public void cart(ActionEvent actionEvent)throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("products.fxml"));
+        anchor.getChildren().setAll(pane);
+    }
+
+    public void porder(ActionEvent actionEvent) throws IOException{
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("porder.fxml"));
+        anchor.getChildren().setAll(pane);
+    }
 }
