07dda09e28bd8843415b068b3a933879583e0c7b
GabrielaZarnescu <zarnescu_gabriela@yahoo.com>
2020-06-15 20:13:32 +0300

    JOSMA-49 fxml created

---
+++ src/main/java/Product.java
@@ -0,0 +1,12 @@
+
+public class Product {
+    private String name;
+    private String price;
+    private String material;
+    private String quantity;
+    private String id;
+    private String picture;
+
+    public Product() {
+    }
+}
---
+++ src/main/java/ViewProductsController.java
@@ -0,0 +1,10 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableColumn;
+
+public class ViewProductsController {
+
+
+
+}
+
